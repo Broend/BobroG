efd4703a18e7c1a7f20f18a176cdbd402e21ae0a
Broend <90361232+Broend@users.noreply.github.com>
2021-09-09 07:14:33 +0700

    BobroG
    ngetes

---
+++ scrpt.js
@@ -0,0 +1,21 @@
+const Keterangan = ['FADLI','ROSYID','DESHILA','THALITHA'];
+let count = 0;
+let txtIndex = 0;
+let currenTxt = '';
+let words = '';
+
+(function ketik(){
+   if(count == Keterangan.length){
+       count = 0;
+   }
+   currenTxt = Keterangan[count];
+   words = currenTxt.slice(0,++ txtIndex);
+
+   document.querySelector('.ngetik').textContent = words;
+   setTimeout(ketik,500);
+
+   if(words.length == currenTxt.length){
+       count++;
+       txtIndex = 0;
+     }
+})();
